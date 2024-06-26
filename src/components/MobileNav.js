5f5b755f2427d4992f73fe8713e1f0ee1aef5cc3
Thato Kalagobe <thatokalagobe@proton.me>
2024-06-26 23:29:05 +0200

    added nav bar + logo

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { IoMdClose } from 'react-icons/io';
+import { CgMenuRight } from 'react-icons/cg';
+import { motion } from 'framer-motion';
 
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  return <nav>
+    {/* nav open button */}
+    <div className='text-3xl cursor-pointer'>
+      <CgMenuRight className='text-2xl' />
+    </div>
+  </nav>;
 };
 
 export default MobileNav;
