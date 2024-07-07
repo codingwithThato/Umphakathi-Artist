9656bdbfd79eea19f9bbdac7a568cd1fe2df30d5
Thato Kalagobe <thatokalagobe@proton.me>
2024-07-07 10:47:42 +0200

    added cursor context but need to fix it

--- src/context/CursorContext.js
+++ src/context/CursorContext.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 
-const CursorContext = () => {
-  return <div>CursorContext</div>;
+export const CursorContext = createContext();
+
+const CursorProvider = ({ children }) => {
+  // cursor position state
+  const [cursorPos, setCursorPos] = useState({ x: 0, y: 0, });
+  
+  useEffect(() => {
+    const onMouseMove = (e) => {
+      setCursorPos({ x: e.clientX, y: e.clientY, });
+    };
+    window.addEventListener('mousemove', onMouseMove);
+    return () => {
+      window.removeEventListener('mousemove', onMouseMove);
+    };
+  });
+  
+  // cursor variants
+  const cursorVariants = {
+    default: {
+      x: cursorPos.x - 16,
+      y: cursorPos.y - 16,
+      backgroundColor: '#0e1112',
+    },
+  };
+
+  return (
+  <CursorContext.Provider value={cursorVariants}>
+    {children}
+  </CursorContext.Provider>
+  );
 };
-
-export default CursorContext;
+ 
+export default CursorProvider;
